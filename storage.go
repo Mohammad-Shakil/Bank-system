7955d7a04adb594876eed952dd97960bfc453747
Shakil <md.fahim.sakil@gmail.com>
2026-09-22 12:41:44 +0600

    function done

--- storage.go
+++ storage.go
@@ -15,3 +15,16 @@ func saveAccount(acc account.Account) error {
 	}
 	return os.WriteFile(datafile, data, 0644)
 }
+
+func loadAccount() account.Account {
+	data, err := os.ReadFile(datafile)
+	if err != nil {
+		return account.Account{}
+	}
+	var acc account.Account
+	err = json.Unmarshal(data, &acc)
+	if err != nil {
+		return account.Account{}
+	}
+	return acc
+}
